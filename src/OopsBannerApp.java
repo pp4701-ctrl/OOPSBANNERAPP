d4460dc9aaec8a9416f3df24067642abf80e12b7
pp4701-ctrl <pp4701@srmist.edu.in>
2026-03-13 09:24:03 +0530

    feature/uc3

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,11 +1,12 @@
 class OopsBannerApp{
      
        public static void main(String[] args){
-        System.out.println("  ***  " + "     ***   "+ " ***   "+"   ***");
-        System.out.println("**    **" + "  **    ** "+ "*  *  "+" * ");
-        System.out.println("**    **" + "  **    ** "+ "***   "+"  **");
-        System.out.println("**    **" + "  **    ** "+ "*     "+"    *");
-        System.out.println("  ***  " + "     ***   "+  "*     "+" *** ");
+     System.out.println(String.join(" ", " *** ", "   ***", " ** ", "   ** "));
+     System.out.println(String.join(" ", "*    *", "*    *", "*  *", "**   "));
+     System.out.println(String.join(" ", "*    *", "*    *", "** ", " * "));
+     System.out.println(String.join(" ", "*    *", "*    *", "*   ", "  **"));
+     System.out.println(String.join(" ", "*    *", "*    *", "*   ", "*  **"));
+     System.out.println(String.join(" ", " ***", "    *** ", "*   ", " *** "));
         
     }
 }
